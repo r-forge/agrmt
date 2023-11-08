b34d4bb62738333e0ae51e66873af16c90a7d237
ruedin (Didier Ruedin) <didier.ruedin@unine.ch>
2023-11-08 00:41:03 +0000

    1.42.12 fixes in collapse and documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/agrmt@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/collapse.R
+++ pkg/R/collapse.R
@@ -1,12 +1,11 @@
 collapse =
-  function(D, pos=FALSE, na.rm=TRUE, na.replace = FALSE) {
+  function(D, pos=FALSE, na.rm=TRUE) {
     # Calculates a frequency vector F from a population vector D
     # Arguments:  D  = population vector
     #            pos = positions of the categories
     #                  (necessary if categories with 0 observations exist)
     #                  if not provided, we assume observations in all categories
     #          na.rm = NA are removed (default)
-    #    na.replaced = NA replaced with a specific value (default off)
     if(na.rm == TRUE) {
       # remove NA from vector, as.numeric() to get back the vector
       D = as.numeric(na.omit(D))
@@ -23,7 +22,8 @@ collapse =
       # fail with warning if NA are found or empty
       warning("Warning: Expected a vector, vector is empty, or vector starts with NA.")
       return(NA)}
-    T = as.data.frame(table(D))  # table(D) to count frequencies
+    if(na.rm==TRUE){T = as.data.frame(table(D))}                  # table(D) to count frequencies
+    else{T = as.data.frame(table(D, useNA="ifany"))}  # table(D) to count frequencies
     F = T[,2]                    # [,2] chooses the values
     l = length(pos)              # number of categories specified
     if (!(pos[1] == FALSE & pos[l] == FALSE)) {   # number of positions is provided:
